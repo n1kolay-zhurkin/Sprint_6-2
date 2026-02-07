f11930e806d8dbd302cd54050323ff82b286b943
n1kolay-zhurkin <zhurkun96@gmail.com>
2026-02-07 16:34:01 +0300

    Добавлены исправления tests/test_order.py и tests/test_logos.py

--- tests/test_logos.py
+++ tests/test_logos.py
@@ -1,12 +1,12 @@
 from pages.main_page import MainPage
 from config import BASE_URL, DZEN_URL_PART
-from selenium.webdriver.support.ui import WebDriverWait
 
 def test_logo_scooter_redirect(driver):
     page = MainPage(driver)
     page.open(BASE_URL)
     page.close_cookie()
     page.click_logo_scooter()
+
     assert page.current_url_contains(BASE_URL)
 
 def test_logo_yandex_redirect(driver):
@@ -16,6 +16,5 @@ def test_logo_yandex_redirect(driver):
     page.click_logo_yandex()
     page.switch_to_last_window()
 
-    # Ждём редирект
-    WebDriverWait(driver, 10).until(lambda d: DZEN_URL_PART in d.current_url)
-    assert DZEN_URL_PART in driver.current_url
+    page.wait_url_contains(DZEN_URL_PART)
+    assert page.current_url_contains(DZEN_URL_PART)
--- tests/test_order.py
+++ tests/test_order.py
@@ -1,11 +1,12 @@
-import pytest
 from pages.main_page import MainPage
 from pages.order_page import OrderPage
 from data.order_data import ORDER_DATA
 from config import BASE_URL
 
-@pytest.mark.parametrize("data", ORDER_DATA)
-def test_order_scooter_top_button(driver, data):
+
+def test_order_top_button_1_day_black(driver):
+    data = ORDER_DATA[0]
+
     main_page = MainPage(driver)
     main_page.open(BASE_URL)
     main_page.close_cookie()
@@ -14,22 +15,34 @@ def test_order_scooter_top_button(driver, data):
     order_page = OrderPage(driver)
     order_page.fill_order_form(data)
     order_page.click_next()
-
-    if data["rent_days"] == 1:
-        order_page.select_rent_1_day()
-    else:
-        order_page.select_rent_3_days()
-
-    if data["color"] == "black":
-        order_page.select_black_color()
-    else:
-        order_page.select_grey_color()
-
+    order_page.select_rent_1_day()
+    order_page.select_black_color()
     order_page.fill_comment_and_order(data["comment"])
+
     assert order_page.is_order_success_popup_visible()
 
-@pytest.mark.parametrize("data", ORDER_DATA)
-def test_order_scooter_bottom_button(driver, data):
+
+def test_order_top_button_3_days_grey(driver):
+    data = ORDER_DATA[1]
+
+    main_page = MainPage(driver)
+    main_page.open(BASE_URL)
+    main_page.close_cookie()
+    main_page.click_order_top()
+
+    order_page = OrderPage(driver)
+    order_page.fill_order_form(data)
+    order_page.click_next()
+    order_page.select_rent_3_days()
+    order_page.select_grey_color()
+    order_page.fill_comment_and_order(data["comment"])
+
+    assert order_page.is_order_success_popup_visible()
+
+
+def test_order_bottom_button_1_day_black(driver):
+    data = ORDER_DATA[0]
+
     main_page = MainPage(driver)
     main_page.open(BASE_URL)
     main_page.close_cookie()
@@ -38,16 +51,26 @@ def test_order_scooter_bottom_button(driver, data):
     order_page = OrderPage(driver)
     order_page.fill_order_form(data)
     order_page.click_next()
-
-    if data["rent_days"] == 1:
-        order_page.select_rent_1_day()
-    else:
-        order_page.select_rent_3_days()
-
-    if data["color"] == "black":
-        order_page.select_black_color()
-    else:
-        order_page.select_grey_color()
-
+    order_page.select_rent_1_day()
+    order_page.select_black_color()
     order_page.fill_comment_and_order(data["comment"])
+
+    assert order_page.is_order_success_popup_visible()
+
+
+def test_order_bottom_button_3_days_grey(driver):
+    data = ORDER_DATA[1]
+
+    main_page = MainPage(driver)
+    main_page.open(BASE_URL)
+    main_page.close_cookie()
+    main_page.click_order_bottom()
+
+    order_page = OrderPage(driver)
+    order_page.fill_order_form(data)
+    order_page.click_next()
+    order_page.select_rent_3_days()
+    order_page.select_grey_color()
+    order_page.fill_comment_and_order(data["comment"])
+
     assert order_page.is_order_success_popup_visible()
